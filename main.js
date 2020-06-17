404302a5d0a4e4fe24b7dd2d143b6f9d3c474acc
amin havsi <pau.ahq@gmail.com>
2020-06-17 10:55:37 +0430

    until handler

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
-const command = process.argv[2];
-if (command === 'add') {
-    console.log('adding');
-} else {
-    console.log('not valid');
-}
+const yargs = require('yargs');
+yargs.command({
+    command: 'create',
+    aliases: ['c', 'ct'],
+    describe: '[create new contact]',
+    builder: {
+        fullname: {
+            alias: 'f',
+            describe: 'person fullname',
+            demandOption: true,
+            type: 'string',
+        },
+        phone: {
+            alias: 'p',
+            describe: 'person phone number',
+            demandOption: true,
+            type: 'number',
+        },
+        email: {
+            alias: 'e',
+            describe: 'person email',
+            demandOption: true,
+            type: 'string',
+        },
+    },
+    handler({ fullname, phone, email }) {
+        console.log(fullname, email, phone);
+    },
+});
+
+yargs.parse();
